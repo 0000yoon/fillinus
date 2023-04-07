27d4458d1776ad40ab0e733ceb3779dd850816ad
0000yoon <jamnea@naver.com>
2023-04-07 16:01:11 +0900

    230407_components_home

---
+++ src/components/BottomActionBar.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function BottomActionBar(){
+    
+}
+
+export default BottomActionBar;
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Main(){
+    
+}
+
+export default Main;
