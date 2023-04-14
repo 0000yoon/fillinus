6aef75c96937c7b746a721602d89ff411047463f
0000yoon <jamnea@naver.com>
2023-04-14 13:33:32 +0900

    230414_add Typography

--- src/components/BottomActionBar.js
+++ src/components/BottomActionBar.js
@@ -1,7 +1,16 @@
 import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 function BottomActionBar(){
-    
+   return(
+
+      <Container sx = {{ bgcolor: '#343434', height: 'auto' }}>
+      <Typography variant="h5" component="h6">
+          BottomActionBar Area
+          </Typography>
+  </Container>
+   );
 }
 
 export default BottomActionBar;
